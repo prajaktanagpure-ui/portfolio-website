b2fa9a6c0188f34a712569f14fe2c45d566768d3
prajaktanagpure-ui <prajaktanagpure@gmail.com>
2020-04-29 00:09:21 -0400

    Update scripts.js

--- PortfolioSite_Prajakta/js/scripts.js
+++ PortfolioSite_Prajakta/js/scripts.js
@@ -1,3 +1,11 @@
 // JavaScript Document
- alert("Hi! Welcome to My Portfolio Site"); 
- console.log("Hello World");
+console.log("Hi! Welcome to My Portfolio Site");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
